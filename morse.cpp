9b1283f4936a69f3640b7071cf8b20fda58b42ac
Andris Laduzans <andris.laduzans@gmail.com>
2022-08-06 10:38:48 +0300

    feat(morse code): started on morse code class

---
+++ morse.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class Morse {
+    private:
+        string decodeChar(char alpha) {
+            switch (day) {
+                case 'a': return ".-";
+                case 'b': return "-...";
+                case 'c': return "-.-.";
+                case 'd': return "-..";
+                case 'e': return ".";
+                case 'f': return "..-.";
+                case 'g': return "--.";
+                case 'h': return "....";
+                case 'i': return "..";
+                case 'j': return ".---";
+                case 'k': return "-.-.";
+                case 'l': return ".-..";
+                case 'm': return "--";
+                case 'n': return "-.";
+                case 'o': return "---";
+                case 'p': return ".--.";
+                case 'q': return "--.-";
+                case 'r': return ".-.";
+                case 's': return "...";
+                case 't': return "-";
+                case 'u': return "..-";
+                case 'v': return "...-";
+                case 'w': return ".--";
+                case 'x': return "-..-";
+                case 'y': return "-.--";
+                case 'z': return "--..";
+                case " ": return "/";
+        }
+    public:
+        string[] input()
+};
+
+int main()
+{
+    
+
+    return 0;
+}
